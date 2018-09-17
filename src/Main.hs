5d31ae1690fa258af3adb328be9e4b28d0de6f6d
Scott Brown <brown131@yahoo.com>
2018-09-16 20:03:51 -0500

    Work on using game state to move pieces.

--- src/Main.hs
+++ src/Main.hs
@@ -2,14 +2,16 @@
 
 module Main where
 
---import Debug.Trace()
+import Debug.Trace (trace)
+import Data.Maybe
 import FRP.Elerea.Simple
 import Graphics.Gloss
 import qualified Graphics.Gloss.Interface.IO.Game as G
 
 data Location = Black
               | White
-              | Free
+              | Empty
+              deriving (Show, Eq)
              
 type InputEvent = G.Event
 
@@ -28,34 +30,65 @@ kewai = [((offset 200), 200),
 putahi :: Point
 putahi = (0, 0)
 
+pieceSize :: Float
+pieceSize = 80
+
+initialGameState :: GameState
+initialGameState = [Black, Black, Black, Black, White, White, White, White, Empty]
+
 offset :: Float -> Float
 offset x = x * tan (pi / 8.0)  -- 45 degrees
 
-renderBoard :: Picture -> Picture 
-renderBoard t = Pictures $ [(lineLoop kewai)] ++
+renderPiece :: Location -> Point -> Picture
+renderPiece loc (x, y) = Translate x y $ Color (if (loc == White) then white else black) $ ThickCircle 1 pieceSize
+
+renderLocation :: Int -> Location -> Picture
+renderLocation _ Empty = Text ""
+renderLocation 8 loc   = renderPiece loc putahi
+renderLocation s loc   = renderPiece loc (kewai !! s)
+                        
+renderBoard :: GameState -> Picture 
+renderBoard g = Pictures $ [(lineLoop kewai)] ++
                 (map (\s -> Line [(kewai !! s), putahi]) [0..7]) ++
-                (map (\s -> Translate (fst $ (kewai !! s)) (snd $ (kewai !! s))
-                            $ Color (if (s > 3) then white else black)$ ThickCircle 20 40) [0..7]) ++
-                [(Translate (-150) 0 $ Scale 0.1 0.1 $ t)]
+                (map (\s -> renderLocation s (g !! s)) [0..8])
   
 renderEvent :: Maybe InputEvent -> Picture
---renderEvent a | trace ("renderEvent " ++ show a) False = undefined   
 renderEvent (Just e) = Text $ show e
 renderEvent Nothing  = Text "Nothing"
 
+-- Determine mouse slot (0..8)
+findSlot :: Point -> Maybe Int
+findSlot (x, y) = listToMaybe $ filter (\s -> let (x', y') = if s == 8 then putahi else kewai !! s
+                                              in pieceSize <= sqrt ((x - x')^2 + ((y - y')^2))) [0..8]
+
+replaceNth :: Int -> a -> [a] -> [a]
+replaceNth _ _ [] = []
+replaceNth n val (x:xs)
+  | n == 0 = val:xs
+  | otherwise = x:replaceNth (n - 1) val xs
+   
+movePiece :: Point -> GameState -> GameState
+--movePiece a  | trace ("movePiece " ++ show a ) False = undefined
+movePiece p g = let emptySlot = filter (\s -> (g !! s) == Empty) [0..8]
+                    slot = findSlot p
+                in [Empty, Black, Black, Black, White, White, White, White, Black]
+  
 network :: Signal (Maybe Float)
         -> Signal (Maybe InputEvent)
+        -> Signal GameState
         -> SignalGen (Signal Picture)
-network _ glossEvent = do
-  sig <- transfer undefined (\e _ -> (renderBoard $ renderEvent e)) glossEvent 
+network _ glossEvent game = do
+  -- TODO: Call movePiece when e is a mouse click event
+  sig <- transfer2 undefined (\e g _ -> (renderBoard g)) glossEvent game
   return sig
 
 main :: IO ()
 main = do
   (tickGen, tickSink) <- external Nothing
   (inputGen, inputSink) <- external Nothing
+  (gameGen, gameSink) <- external $ initialGameState
     
-  recomputePicture <- start $ network tickGen inputGen
+  recomputePicture <- start $ network tickGen inputGen gameGen
   initialPicture <- recomputePicture
     
   G.playIO (InWindow "Mu Torere" (500, 500) (0, 0))
@@ -64,7 +97,9 @@ main = do
     initialPicture
     return
     (\e _ -> do
-        inputSink (Just e)
+        case e of
+          (G.EventKey (G.MouseButton G.LeftButton) G.Up _ p) -> inoutSink (just e) --gameSink $ movePiece p
+          _ -> inputSink (Just e)
         recomputePicture)
     (\t _ -> do
         tickSink (Just t)
