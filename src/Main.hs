f5160c26d82cd2d9fb5499a55bf300a76ef4d151
Scott Brown <brown131@yahoo.com>
2018-09-22 03:53:55 -0500

    Eliminated warnings

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,6 @@
 
 module Main where
 
-import Control.Monad.State.Lazy
-import Debug.Trace (trace)
 import Data.Maybe
 import FRP.Elerea.Simple
 import Graphics.Gloss
@@ -15,8 +13,6 @@ data Location = Black
               deriving (Show, Eq)
              
 type InputEvent = G.Event
-
-type GameState a = StateT [Location] IO a
 
 offset :: Float -> Float
 offset x = x * tan (pi / 8.0)  -- 45 degrees
@@ -37,12 +33,8 @@ putahi = (0, 0)
 pieceSize :: Float
 pieceSize = 80
 
-locations :: [Location]
-locations = [Black, Black, Black, Black, White, White, White, White, Empty]
-
-initialGameState :: GameState ()
-initialGameState = do
-  put locations
+initialLocations :: [Location]
+initialLocations = [Black, Black, Black, Black, White, White, White, White, Empty]
 
 renderPiece :: Location -> Point -> Picture
 renderPiece loc (x, y) = Translate x y $ Color (if (loc == White) then white else black) $ ThickCircle 1 pieceSize
@@ -56,10 +48,9 @@ renderEvent :: Maybe InputEvent -> Picture
 renderEvent (Just e) = Text $ show e
 renderEvent Nothing  = Text "Nothing"
 
--- Determine mouse slot (0..8)
 findSlot :: Point -> Maybe Int
 findSlot (x, y) = listToMaybe $ filter (\s -> let (x', y') = if s == 8 then putahi else kewai !! s
-                                              in sqrt ((x - x')^2 + ((y - y')^2)) <= pieceSize) [0..8] 
+                                              in sqrt ((x - x')**2 + ((y - y')**2)) <= pieceSize) [0..8] 
 
 findSelectedSlot :: Maybe InputEvent -> Maybe Int
 findSelectedSlot (Just (G.EventKey (G.MouseButton G.LeftButton) G.Down _ p)) = findSlot p
@@ -77,21 +68,18 @@ renderBoard g = Pictures $ [(lineLoop kewai)] ++
                   (map (\s -> renderLocation s (g !! s)) [0..8])
 
 movePiece :: Maybe InputEvent -> [Location] -> [Location]
---movePiece a b | trace ("movePiece " ++ show b ) False = undefined
 movePiece e g = do
                 let emptySlot = head $ filter (\s -> (g !! s) == Empty) [0..8]
                 let slot = findSelectedSlot e
                 case slot of
                   Just s  -> replaceNth emptySlot (g !! s) $ replaceNth s Empty g
                   Nothing -> g
-                  
---prerenderBoard :: 
 
 network :: Signal (Maybe Float)
         -> Signal (Maybe InputEvent)
         -> SignalGen (Signal Picture)
 network _ glossEvent = do
-  newGame <- transfer locations  (\e g -> movePiece e g) glossEvent
+  newGame <- transfer initialLocations (\e g -> movePiece e g) glossEvent
   return $ renderBoard <$> newGame
 
 main :: IO ()
