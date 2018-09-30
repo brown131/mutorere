9ad49ed235ae7ec0a8e1a8de2b4ace236e1810e7
Scott Brown <brown131@yahoo.com>
2018-09-30 09:24:38 -0500

    Changes LocationValue to a Maybe Stone.

--- src/Main.hs
+++ src/Main.hs
@@ -7,13 +7,12 @@ import FRP.Elerea.Simple
 import Graphics.Gloss
 import qualified Graphics.Gloss.Interface.IO.Game as G
 
-data LocationValue = Black
-                   | White
-                   | Empty
-                   deriving (Show, Eq)
+data Stone = Black | White deriving (Show, Eq)
+
+type LocationValue = Maybe Stone
 
 data GameState = GameState { locations :: [LocationValue],
-                             move      :: LocationValue }
+                             move      :: Stone }
                  
 type InputEvent = G.Event
 
@@ -37,7 +36,8 @@ pieceSize :: Float
 pieceSize = 80
 
 initialGameState :: GameState
-initialGameState = GameState { locations = [Black, Black, Black, Black, White, White, White, White, Empty],
+initialGameState = GameState { locations = [Just Black, Just Black, Just Black, Just Black,
+                                            Just White, Just White, Just White, Just White, Nothing],
                                move = Black }
 
 clickedLocation :: Point -> Maybe Int
@@ -52,15 +52,15 @@ getVal :: GameState -> Int -> LocationValue
 getVal g i = (locations g) !! i
 
 movableLocation :: Maybe Int -> GameState -> Maybe Int
-movableLocation (Just 8) g | (getVal g 8) == (move g) = (Just 8)
-movableLocation (Just i) g | (getVal g i) == (move g) &&
+movableLocation (Just 8) g | (getVal g 8) == Just (move g) = (Just 8)
+movableLocation (Just i) g | (getVal g i) == Just (move g) &&
                              -- Must be next to an empty location.
-                             ((getVal g 8) == Empty ||
-                               (getVal g ((i + 1) `mod` 8)) == Empty ||
-                               (getVal g ((i + 7) `mod` 8)) == Empty) &&
+                             ((getVal g 8) == Nothing ||
+                               (getVal g ((i + 1) `mod` 8)) == Nothing ||
+                               (getVal g ((i + 7) `mod` 8)) == Nothing) &&
                              -- Cannot move if a player's own kewai are on both sides.
-                             not ((getVal g ((i + 1) `mod` 8)) == (move g) &&
-                                   (getVal g ((i + 7) `mod` 8)) == (move g)) = (Just i)
+                             not ((getVal g ((i + 1) `mod` 8)) == Just (move g) &&
+                                  (getVal g ((i - 1) `mod` 8)) == Just (move g)) = (Just i)
 movableLocation _ _ = Nothing 
 
 setLocation :: Int -> LocationValue -> [LocationValue] -> [LocationValue]
@@ -69,15 +69,15 @@ setLocation n val (x:xs)
   | n == 0 = val:xs
   | otherwise = x:setLocation (n - 1) val xs
 
-nextMove :: GameState -> LocationValue
+nextMove :: GameState -> Stone
 nextMove g | (move g) == Black = White
            | otherwise = Black
 
 renderPiece :: LocationValue -> Point -> Picture
-renderPiece loc (x, y) = Translate x y $ Color (if (loc == White) then white else black) $ ThickCircle 1 pieceSize
+renderPiece loc (x, y) = Translate x y $ Color (if (loc == Just White) then white else black) $ ThickCircle 1 pieceSize
 
 renderLocation :: Int -> LocationValue -> Picture
-renderLocation _ Empty = Text ""
+renderLocation _ Nothing = Text ""
 renderLocation 8 loc   = renderPiece loc putahi
 renderLocation i loc   = renderPiece loc (kewai !! i)
 
@@ -94,15 +94,15 @@ renderBoard g = Pictures $ [(lineLoop kewai)] ++
                   (renderStatusBar g)
 
 gameOver :: GameState -> Bool
-gameOver g = (find (\i -> ((getVal g i) == (move g)) &&
+gameOver g = (find (\i -> ((getVal g i) == Just (move g)) &&
                           (not ((movableLocation (Just i) g) == Nothing))) [0..8]) == Nothing
   
 movePiece :: Maybe InputEvent -> GameState -> GameState
 movePiece e g = do
-                  let emptyLocation = head $ filter (\i -> (getVal g i) == Empty) [0..8]
+                  let openLocation = head $ filter (\i -> (getVal g i) == Nothing) [0..8]
                   case (movableLocation (findLocation e) g) of
-                    Just i  -> g { locations = (setLocation emptyLocation (getVal g i) $
-                                                 setLocation i Empty (locations g)),
+                    Just i  -> g { locations = (setLocation openLocation (getVal g i) $
+                                                 setLocation i Nothing (locations g)),
                                    move = (nextMove g) }
                     Nothing -> g
 
@@ -119,8 +119,7 @@ main = do
   (tickGen, tickSink) <- external Nothing
   (inputGen, inputSink) <- external Nothing
     
-  recomputePicture <- start $ do
-    network tickGen inputGen
+  recomputePicture <- start $ do network tickGen inputGen
     
   initialPicture <- recomputePicture
     
@@ -135,3 +134,4 @@ main = do
     (\t _ -> do
         tickSink (Just t)
         recomputePicture)
+    
