78ab815053042fe06454e363fa30215b5a74d620
Scott Brown <brown131@yahoo.com>
2018-09-30 16:23:31 -0500

    Got rid of extra perentheses

--- src/Main.hs
+++ src/Main.hs
@@ -42,7 +42,7 @@ initialGameState = GameState { locations = [Just Black, Just Black, Just Black, 
 
 clickedLocation :: Point -> Maybe Int
 clickedLocation (x, y) = listToMaybe $ filter (\i -> let (x', y') = if i == 8 then putahi else kewai !! i
-                                              in sqrt ((x - x')**2 + ((y - y')**2)) <= (pieceSize / 2)) [0..8] 
+                                              in sqrt ((x - x') ** 2 + ((y - y') ** 2)) <= pieceSize / 2) [0..8] 
 
 findLocation :: Maybe InputEvent -> Maybe Int
 findLocation (Just (G.EventKey (G.MouseButton G.LeftButton) G.Down _ p)) = clickedLocation p
@@ -52,15 +52,15 @@ getVal :: GameState -> Int -> LocationValue
 getVal g i = (locations g) !! i
 
 movableLocation :: Maybe Int -> GameState -> Maybe Int
-movableLocation (Just 8) g | (getVal g 8) == Just (move g) = (Just 8)
-movableLocation (Just i) g | (getVal g i) == Just (move g) &&
-                             -- Must be next to an empty location.
-                             ((getVal g 8) == Nothing ||
-                               (getVal g ((i + 1) `mod` 8)) == Nothing ||
-                               (getVal g ((i + 7) `mod` 8)) == Nothing) &&
+movableLocation (Just 8) g | getVal g 8 == Just (move g) = (Just 8)
+movableLocation (Just i) g | getVal g i == Just (move g) &&
+                             -- Must be next to an open location.
+                             (getVal g 8 == Nothing ||
+                              getVal g ((i + 1) `mod` 8) == Nothing ||
+                              getVal g ((i - 1) `mod` 8) == Nothing) &&
                              -- Cannot move if a player's own kewai are on both sides.
-                             not ((getVal g ((i + 1) `mod` 8)) == Just (move g) &&
-                                  (getVal g ((i - 1) `mod` 8)) == Just (move g)) = (Just i)
+                             not (getVal g ((i + 1) `mod` 8) == Just (move g) &&
+                                  getVal g ((i - 1) `mod` 8) == Just (move g)) = Just i
 movableLocation _ _ = Nothing 
 
 setLocation :: Int -> LocationValue -> [LocationValue] -> [LocationValue]
@@ -70,11 +70,11 @@ setLocation n val (x:xs)
   | otherwise = x:setLocation (n - 1) val xs
 
 nextMove :: GameState -> Stone
-nextMove g | (move g) == Black = White
+nextMove g | move g == Black = White
            | otherwise = Black
 
 renderPiece :: LocationValue -> Point -> Picture
-renderPiece loc (x, y) = Translate x y $ Color (if (loc == Just White) then white else black) $ ThickCircle 1 pieceSize
+renderPiece loc (x, y) = Translate x y $ Color (if loc == Just White then white else black) $ ThickCircle 1 pieceSize
 
 renderLocation :: Int -> LocationValue -> Picture
 renderLocation _ Nothing = Text ""
@@ -83,27 +83,27 @@ renderLocation i loc   = renderPiece loc (kewai !! i)
 
 renderStatusBar :: GameState -> [Picture]
 renderStatusBar g = [Translate (-250) (-290) $ Color white $ Polygon [(0, 0), (0, 30), (500, 30), (500, 0)]] ++
-                    [Translate (-240) (-280) $ Scale 0.1 0.1 $ Text $ if (gameOver g)
-                                                                      then (show (nextMove g)) ++ " wins!"
-                                                                      else ("Move: " ++ (show $ move g))]
+                    [Translate (-240) (-280) $ Scale 0.1 0.1 $ Text $ if gameOver g
+                                                                      then (show $ nextMove g) ++ " wins!"
+                                                                      else "Move: " ++ (show $ move g)]
 
 renderBoard :: GameState -> Picture 
-renderBoard g = Pictures $ [(lineLoop kewai)] ++
-                  (map (\i -> Line [(kewai !! i), putahi]) [0..7]) ++
-                  (map (\i -> renderLocation i (getVal g i)) [0..8]) ++
-                  (renderStatusBar g)
+renderBoard g = Pictures $ [lineLoop kewai] ++
+                  map (\i -> Line [kewai !! i, putahi]) [0..7] ++
+                  map (\i -> renderLocation i (getVal g i)) [0..8] ++
+                  renderStatusBar g
 
 gameOver :: GameState -> Bool
-gameOver g = (find (\i -> ((getVal g i) == Just (move g)) &&
-                          (not ((movableLocation (Just i) g) == Nothing))) [0..8]) == Nothing
+gameOver g = find (\i -> getVal g i == Just (move g) &&
+                         not (movableLocation (Just i) g == Nothing)) [0..8] == Nothing
   
 movePiece :: Maybe InputEvent -> GameState -> GameState
 movePiece e g = do
                   let openLocation = head $ filter (\i -> (getVal g i) == Nothing) [0..8]
-                  case (movableLocation (findLocation e) g) of
-                    Just i  -> g { locations = (setLocation openLocation (getVal g i) $
-                                                 setLocation i Nothing (locations g)),
-                                   move = (nextMove g) }
+                  case movableLocation (findLocation e) g of
+                    Just i  -> g { locations = setLocation openLocation (getVal g i) $
+                                               setLocation i Nothing (locations g),
+                                   move = nextMove g }
                     Nothing -> g
 
 network :: SignalGen (Signal (Maybe Float))
@@ -129,9 +129,9 @@ main = do
     initialPicture
     return
     (\e _ -> do
-        inputSink (Just e)
+        inputSink $ Just e
         recomputePicture)
     (\t _ -> do
-        tickSink (Just t)
+        tickSink $ Just t
         recomputePicture)
     
